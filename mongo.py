a607d163d4d3261f30806d580018eb4cf22512de
Samith Shetty <samithshetty0@gmail.com>
2026-01-02 14:18:37 -0500

    Fixed Formatting

--- mongo.py
+++ mongo.py
@@ -1,18 +1,17 @@
-
-from pymongo import MongoClient
-from config import MONGODB_URI
-from models import Stats, Hero
 from datetime import datetime, timezone
 from typing import List
 
+from pymongo import MongoClient
 
+from config import MONGODB_URI
+from models import Hero, Stats
 
 client = MongoClient(MONGODB_URI)
-database = client['DunGenDB']
-hero_db = database['Heroes']
+database = client["DunGenDB"]
+hero_db = database["Heroes"]
 
 # Indexes
-hero_db.create_index('owner_id')
+hero_db.create_index("owner_id")
 
 
 def create_hero_for_user(user_id: int, name: str, stats: Stats, level: int) -> Hero:
@@ -22,17 +21,16 @@ def create_hero_for_user(user_id: int, name: str, stats: Stats, level: int) -> H
         stats=stats,
         HP=stats.MAX_HP,
         owner_id=user_id,
-        createdAt=datetime.now(tz=timezone.utc)
+        createdAt=datetime.now(tz=timezone.utc),
     )
     new_hero_doc = new_hero.model_dump(by_alias=True, exclude_none=True)
     hero_db.insert_one(new_hero_doc)
     return new_hero
-    
+
 
 def get_heroes_for_user(user_id: int) -> List[Hero]:
-    cursor = hero_db.find({'owner_id': str(user_id)})
-    return [[Hero.model_validate(doc) for doc in cursor]]
-
+    cursor = hero_db.find({"owner_id": str(user_id)})
+    return [Hero.model_validate(doc) for doc in cursor]
 
 
 def update_hero():
